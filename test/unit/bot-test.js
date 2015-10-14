f4fdf3024d297847741bdabddd522d5f525c920a
Erik Aybar <erikthedeveloper@gmail.com>
2015-10-14 01:51:11 -0600

    Removes/Replaces deprecated bot-test.js w/ bot-useful-test.js

--- test/unit/bot-test.js
+++ test/unit/bot-test.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var expect = require('chai').expect;
 var sinon = require('sinon');
 var http = require('http');
@@ -6,39 +7,43 @@ var PassThrough = require('stream').PassThrough;
 
 var bot = require('./../../bot');
 
-describe('Math Bot', function () {
-  // TODO: This is basically garbage...
-
-  var botInstance;
+describe('MathBot - Requester of Mathematical Solutions', function() {
   var stubbedRequest;
+  var dummyRequest;
+  var dummyResponse;
 
   beforeEach(function() {
     stubbedRequest = sinon.stub(http, 'request');
-    botInstance = bot('TestBot');
+    dummyResponse = new PassThrough();
+    dummyRequest = new PassThrough();
   });
 
   afterEach(function() {
     stubbedRequest.restore();
   });
 
-  it('Requests solution to expression and does that funky thing...', function(done) {
+  it('Accepts options and is humanized via "name"', function () {
+    expect(bot().name).to.match(/BOT_/, 'Should have default/generate name');
+    expect(bot({name: 'TestBot'}).name).to.equal('TestBot');
+    expect(bot({client: http}).client).to.equal(http);
+    expect(bot({foo: 'bar'}).foo, 'Only whitelist options!').to.be.undefined;
+  });
 
+  it('should request a "solution" to an "expression" via HTTP "/evaluate"', function(done) {
     var expression = '1+1=';
     var expectedSolution = '2';
-    var expectedResponse = new PassThrough();
-    var request = new PassThrough();
-    request.write('foo');
+    var spyRequestWrite = sinon.spy(dummyRequest, 'write');
 
-    expectedResponse.end(expectedSolution);
+    dummyResponse.end(expectedSolution);
 
     stubbedRequest
-      .callsArgWith(1, expectedResponse)
-      .returns(request);
+      .callsArgWith(1, dummyResponse, 'request({}, cb) cb is called w/ Stream')
+      .returns(dummyRequest);
 
-    botInstance.askMath(expression, function(solution) {
-      expect(solution).to.equal(expectedSolution);
+    bot({client: http}).askMath(expression, function(solution) {
+      expect(solution).to.equal(expectedSolution, 'Expected solution {string} read from response Stream');
+      sinon.assert.calledWith(spyRequestWrite, expression);
       done();
     });
-
   });
 });
